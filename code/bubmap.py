ec733779de27eb60a81ef39d457ca2d21cb1a9cd
Enoch K <enochk@Enochs-MBP.attlocal.net>
2018-08-14 23:23:22 -0500

    changed code

--- code/bubmap.py
+++ code/bubmap.py
@@ -1,45 +1,51 @@
+import folium
+from itertools import chain
 import overpy
+import re
 import warnings
 warnings.filterwarnings(action='ignore', category=RuntimeWarning)
 import pandas as pd
-import plotly.plotly as py
-import folium
-import re
-from itertools import chain
 
 def overpyq(loc):
 	quer = '''
 		area["ISO3166-1"="HK"][admin_level=2];
-		(node["name:en"="'''+loc+'''"](area);
+		(node["name:en"="'''+str(loc)+'''"](area);
 		);
 		out center;
 		'''
 	res = api.query(quer)
+	if not res.nodes:
+		quer2 = '''
+			area["ISO3166-1"="HK"][admin_level=2];
+			(way["name:en"="'''+str(loc)+'''"](area);
+			);
+			out center;
+			'''
+		res = api.query(quer2).ways[0].get_nodes(resolve_missing=True)[0] if api.query(quer2).ways != [] else 999
+		return res
+	res = api.query(quer).nodes[0]
 	return res
 
-#read data
+# read data
 filename='Property price chart _ Midland Realty'
 #filename='test'
 data = pd.read_csv('./data/'+filename+'.csv', low_memory=False)
 
-#preprocessing
+# preprocessing
 places = []
 prices = []
 places += [place for place in data.estate]
 prices += list(chain.from_iterable(map(int, re.findall(r'\d+',price.replace(',', ''))) for price in data.avg_price_saleable))
 querydict = dict(zip(places,prices))
 
-# Collect coords into list
+# collect coords into list
 api = overpy.Overpass()
 coords  = []
 lons = []
 lats = []
 for key in querydict:
 	r = overpyq(str(key))
-	if len(r.nodes)==0:
-		coords += [(9999,9999)]
-	else:
-	    coords += [(float(r.nodes[0].lon), float(r.nodes[0].lat))]
+	coords += [(float(r.lon), float(r.lat))] if r != 999 else [(float(999), float(999))]
 lons += [lon[1] for lon in coords]
 lats += [lat[0] for lat in coords]
 
@@ -48,22 +54,21 @@ querydf = pd.DataFrame(querydict.items(), columns=['estate', 'saleable_price'])
 querydf['lon'] = lons
 querydf['lat'] = lats
 
-#create folium map
+# create folium map
 m = folium.Map(location=[22.34, 114.1], tiles='cartodbpositron', zoom_start=10.5)
 folium.TileLayer('cartodbdark_matter').add_to(m)
 folium.TileLayer('stamenterrain').add_to(m)
 folium.TileLayer('stamentoner').add_to(m)
 folium.LayerControl().add_to(m)
-#cartodbpositron
 for i in range(0,len(querydf)):
-	col = '#FF3333' if querydf.iloc[i]['saleable_price'] > 15000 else ('#FF6633' if querydf.iloc[i]['saleable_price'] > 10000 else '#FF9933')
+	col = '#FF0033' if querydf.iloc[i]['saleable_price'] > 15000 else ('#FF6633' if querydf.iloc[i]['saleable_price'] > 10000 else '#FFCC33')
 	folium.Circle(
 	  location=[querydf.iloc[i]['lon'], querydf.iloc[i]['lat']],
 	  popup=querydf.iloc[i]['estate']+': '+str(querydf.iloc[i]['saleable_price'])+' HKD per sq ft',
-	  radius=querydf.iloc[i]['saleable_price']/20,
+	  radius=querydf.iloc[i]['saleable_price']/25,
 	  color=col,
 	  fill=True,
-	  opacity= 0.875,
+	  opacity= 0.89,
 	  fill_color=col,
 	  stroke= True,
 	  weight= 1
